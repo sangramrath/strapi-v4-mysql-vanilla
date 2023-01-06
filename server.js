0336fdf0a990bd21ace90bce45fa580f7ae9565b
sangramrath <sangram.rath@gmail.com>
2023-01-06 15:42:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 'use strict';
 const strapi = require('./node_modules/@strapi/strapi/lib/index');
 
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+});
+
 strapi().start();
 
