ab6b7a542c3b855064e61044a65cd95b3a35e47e
sangramrath <sangram.rath@gmail.com>
2023-01-06 09:48:20 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', 'DB_HOST'),
-      port: env.int('DATABASE_PORT', DB_PORT),
-      database: env('DATABASE_NAME', 'DB_NAME'),
-      user: env('DATABASE_USERNAME', 'DB_USER'),
-      password: env('DATABASE_PASSWORD', 'DB_PASS'),
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'strapi'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
